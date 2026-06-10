9e8887d639b5571db43fc0c72787c5dafa16f267
Blumi <ankur.nairit@gmail.com>
2026-06-10 12:09:53 -0500

    feat(p5): web nikto tool with tuning + text parse

--- packages/tools/src/web/index.ts
+++ packages/tools/src/web/index.ts
@@ -1,1 +1,2 @@
 export { nucleiTool, parseNucleiNdjson, NucleiInput, NucleiOutput, type NucleiInputT, type NucleiOutputT } from './nuclei';
+export { niktoTool, parseNiktoText, NiktoInput, NiktoOutput, type NiktoInputT, type NiktoOutputT } from './nikto';
